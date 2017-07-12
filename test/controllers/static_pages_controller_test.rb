e067570a49b908e16f2affb62055883955f21659
holdercp <holdercp@icloud.com>
2017-07-11 21:54:31 -0400

    Add static pages tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "when root url is visited" do
+    get root_path
     assert_response :success
+    assert_select 'title', 'Home | Longhouse'
+  end
+  test "should get home" do
+    get home_path
+    assert_response :success
+    assert_select 'title', 'Home | Longhouse'
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
+    assert_select 'title', 'About | Longhouse'
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
+    assert_select 'title', 'Contact | Longhouse'
   end
 
 end
