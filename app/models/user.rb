322454acfb1b0b5c937cd189284f1829d07e86a5
holdercp <holdercp@icloud.com>
2017-07-13 11:29:58 -0400

    Add user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Password validations handled by devise gem
+  # Email presence, format, uniqueness handled by devise gem
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, length: { maximum: 255 }
+
 end
