3b2fc2125ba76152146c811c95c75fa3cf99fb25
holdercp <holdercp@icloud.com>
2017-07-13 21:23:05 -0400

    Redirect users to profile after successful login or signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#home'
 
   get '/home', to: 'static_pages#home'
-
   get '/about', to: 'static_pages#about'
-
   get '/contact', to: 'static_pages#contact'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
+
+  root 'static_pages#home'
 end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::ControllerHelpers
+
+  def setup
+    @user = users(:anakin)
+  end
+
+  test "should get show" do
+    sign_in @user
+    get user_path(@user)
+    assert_response :success
+  end
+end
